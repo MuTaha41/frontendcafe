df8a52d000fab0d5bc90cbcc3044655163dc5e1a
Basjabbie <100725442+Basjabbie@users.noreply.github.com>
2024-03-14 15:33:35 +0000

    2

--- src/api/actions.ts
+++ src/api/actions.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from "axios";
 
-const API_URL = "https://localhost:3000/api";
+const API_URL = "https://psychic-halibut-xgxxr66gvrqf6pxp-3000.app.github.dev/api";
 
 export const getWeatherData = async (city: string): Promise<WeatherData> => {
   return new Promise<WeatherData>((resolve, reject) => {
