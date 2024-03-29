7b1b0c319f62ba7d36b74d1ee82be8cc004b94f4
Basjabbie <100725442+Basjabbie@users.noreply.github.com>
2024-03-29 02:20:50 +0000

    food

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
-import WeatherCard from "./components/WeatherCard";
+import RestaurantCard from "./components/RestaurantCard"; // Renamed from WeatherCard
 
 const App = () => {
   return (
     <div className="flex flex-col h-screen">
       <NavBar />
       <div className="flex flex-1 overflow-auto flex-col items-center align-middle justify-center  h-full w-full">
-        <WeatherCard />
+        {/* Assuming that RestaurantCard is now expecting restaurant data */}
+        <RestaurantCard />
       </div>
     </div>
   );
