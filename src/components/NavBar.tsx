f6ff7f18d3f836ed3f041529c078b49aca4ff248
MuTaha41 <125828204+MuTaha41@users.noreply.github.com>
2024-04-30 13:26:44 +0000

    changes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,7 +7,7 @@ const NavBar = () => {
     <Navbar className="bg-slate-100 h-16">
       <NavbarBrand>
         <MdRestaurant className="w-8 h-8 text-primary" /> {/* Updated icon */}
-        <p className="font-bold text-inherit">Restaurant Finder</p> {/* Updated text */}
+        <p className="font-bold text-inherit">CoffeeShop Finder</p> {/* Updated text */}
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         {/* Here you can add links to different parts of your application if needed */}
