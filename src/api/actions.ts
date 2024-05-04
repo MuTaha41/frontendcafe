78524abb125f9e2787dbeea0ce13ae84c14f04f8
MuTaha41 <125828204+MuTaha41@users.noreply.github.com>
2024-05-04 18:48:53 +0000

    change

--- src/api/actions.ts
+++ src/api/actions.ts
@@ -1,7 +1,7 @@
 //MuTaha41//
 import axios, { AxiosError } from "axios";
  
-const API_URL = "https://effective-giggle-666rrpjv74725gxr-3000.app.github.dev/api";
+const API_URL = "https://effective-couscous-9vvqqxp6546hpjj7-3000.app.github.dev/api";
  
 export const getDrinkData = async (drinkType: string): Promise<DrinkData> => {
   return new Promise<DrinkData>((resolve, reject) => {
